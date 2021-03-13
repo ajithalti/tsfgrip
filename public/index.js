c06b59c985fd1fd0dc095045e00eedbe0f83db5a
ajithalti <altiajay2@gmail.com>
2021-03-13 13:39:15 +0530

    h

---
+++ public/index.js
@@ -0,0 +1,29 @@
+
+let ts=0;
+window.onload=function()
+{
+    
+}
+let hm=0;
+function hmenu()
+{
+    if(hm%2==0)
+    {
+        document.getElementById("hc").style.display="block";
+        
+    }
+    else
+    {
+        document.getElementById("hc").style.display="none";
+    }
+    hm++;
+}
+function theme()
+{
+    
+}
+function customers()
+{
+    
+}
+function 
