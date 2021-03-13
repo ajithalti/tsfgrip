09937500a6a2d1c6c7b239bd9065f3cce073c1dc
ajithalti <altiajay2@gmail.com>
2021-03-13 16:53:11 +0530

    guu

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-const express = require('express')
+let express = require('express')
 const path = require('path')
-let bp=require('body-parser');
+let BodyParser=require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
+let cors=require('cors');
 const PORT = process.env.PORT || 5000
 let app=express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(BodyParser.json());                                app.use(BodyParser.urlencoded({ extended: true }));
+
 url="mongodb+srv://AJITH:ajay-ALTI-8@cluster0.9nz68.mongodb.net/?retryWrites=true&w=majority";
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${i PORT }`))
-MongoClient.connect(url, function(err, db) {
+  .get('/', (req, res) => {res.render('pages/index');
+              console.log("ready");})
+  .listen(PORT, () => console.log(`Listening on ${PORT }`))
+/*ongoClient.connect(url, function(err, db) {
        if (err) throw err;
        let dbo=db.db("WT");
        dbo.collection("bankc").insert({"name":"Ajith","bal":"10000","mail":"ajith@gmail.com"},(err,result)=>               {                                                            console.log(result);
        });
+});*/
+app.get('/test',(req,res)=>{
+	res.write("Hello");
 });
-app.get("/viewc",(req,res)=>{
-    MongoClient.connect(url, function(err, db) {
+express().get('/viewc',(req,res)=>{
+	console.log('ffg');
+	res.send("dddd");
+   /* MongoClient.connect(url, function(err, db) {
        if (err) throw err;
        let dbo=db.db("WT");
-       dbo.collection("bankc").find((err,result)=>               {                                                            console.log(result);
-      res.send(JSON.stringify(result));                           });                                                     });
+
+       dbo.collection("bankc").findOne({'name':'Ajith'},(err,result)=>               {                                                            console.log(result);
+                            res.send(JSON.stringify(result));
+	});                                                     });*/
 });
 
 
--- public/client.js
+++ public/client.js
@@ -24,6 +24,24 @@ function theme()
 }
 function customers()
 {
-    
+	alert("hello");
+   let xhr=new XMLHttpRequest();
+	xhr.onreadystatechange=function()
+	{
+		if(this.status==200&&this.readyState==4)
+		{
+			alert("res");
+			if(this.responseText)
+			{
+
+				document.getElementById("content").innerHTML=JSON.parse(this.responseText);
+			}
+		}
+	}
+	xhr.open("GET","viewc",True);
+	xhr.send();
+
+
+
 }
  
