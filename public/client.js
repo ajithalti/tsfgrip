6a26981384e8e680c4897b3517df0fdda247f5a5
ajithalti <altiajay2@gmail.com>
2021-03-14 10:01:20 +0530

    vhj

--- public/client.js
+++ public/client.js
@@ -34,14 +34,67 @@ function customers()
 			if(this.responseText)
 			{
 
-				document.getElementById("content").innerHTML=JSON.parse(this.responseText);
+			       //	document.getElementById("content").innerHTML=
+					let res=JSON.parse(this.responseText);
+			
+				viewc(res);
+				
 			}
 		}
 	}
-	xhr.open("GET","viewc",True);
+	xhr.open("GET","viewc",true);
 	xhr.send();
 
 
 
 }
- 
+function viewc(ip)
+{
+	dlt();
+	let j=0;
+	let content=document.getElementById("content");
+
+	for(i in ip)
+	{
+	
+		let c=document.createElement("div");
+		c.id=j.toString();
+		c.style.border="1px solid green";
+		c.style.backgroundColor="yellow";
+		let name=document.createElement("h3");
+		name.innerHTML="NAME: "+(ip[i].name);
+		
+		let bal=document.createElement("h4");
+		bal.innerHTML="BALANCE: "+ip[i].bal;
+		let mail=document.createElement("h4");
+		mail.innerHTML="MAIL: "+ip[i].mail;
+		let no=document.createElement("h4");
+		no.innerHTML="ACC NO: "+ip[i].no;
+		
+		content.appendChild(document.createElement("br"));
+	//	c.appendChild(document.createElement("br"));
+		c.appendChild(name);
+	//	c.appendChild(document.createElement("br"));
+	//	c.appendChild(no);
+	//	c.appendChild(document.createElement("br"));
+		c.appendChild(mail);
+	//	c.appendChild(document.createElement("br"));
+	//	c.appendChild(bal);
+		content.appendChild(c);;
+
+	}
+}
+
+
+function dlt()
+{
+/*	while(document.getElementById("content").hasChild)
+	{
+		document.getElementById('content').firstChild.remove();
+	}*/
+}
+
+
+
+
+
